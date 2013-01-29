df5a10ba935f65b7b650ffd272e90a58e22232fe
Hector Horta <shinsotua@gmail.com>
2013-01-29 17:27:39 -0300

    Cambios en botones

--- Lifecount/ViewController.h
+++ Lifecount/ViewController.h
@@ -20,6 +20,26 @@
     IBOutlet UILabel *p3LivesView;
     IBOutlet UILabel *p4LivesView;
     
+    UIButton *p1PlusFiveBtn;
+    UIButton *p1MinusFiveBtn;
+    UIButton *p1PlusOneBtn;
+    UIButton *p1MinusOneBtn;
+    
+    UIButton *p2PlusFiveBtn;
+    UIButton *p2MinusFiveBtn;
+    UIButton *p2PlusOneBtn;
+    UIButton *p2MinusOneBtn;
+    
+    UIButton *p3PlusFiveBtn;
+    UIButton *p3MinusFiveBtn;
+    UIButton *p3PlusOneBtn;
+    UIButton *p3MinusOneBtn;
+    
+    UIButton *p4PlusFiveBtn;
+    UIButton *p4MinusFiveBtn;
+    UIButton *p4PlusOneBtn;
+    UIButton *p4MinusOneBtn;
+    
 }
 
 - (IBAction)plusOneP1:(id)sender;
@@ -43,5 +63,25 @@
 - (IBAction)minusFiveP4:(id)sender;
 
 - (IBAction)reset:(id)sender;
+
+@property(retain) IBOutlet UIButton *p1PlusFiveBtn;
+@property(retain) IBOutlet UIButton *p1MinusFiveBtn;
+@property(retain) IBOutlet UIButton *p1PlusOneBtn;
+@property(retain) IBOutlet UIButton *p1MinusOneBtn;
+
+@property(retain) IBOutlet UIButton *p2PlusFiveBtn;
+@property(retain) IBOutlet UIButton *p2MinusFiveBtn;
+@property(retain) IBOutlet UIButton *p2PlusOneBtn;
+@property(retain) IBOutlet UIButton *p2MinusOneBtn;
+
+@property(retain) IBOutlet UIButton *p3PlusFiveBtn;
+@property(retain) IBOutlet UIButton *p3MinusFiveBtn;
+@property(retain) IBOutlet UIButton *p3PlusOneBtn;
+@property(retain) IBOutlet UIButton *p3MinusOneBtn;
+
+@property(retain) IBOutlet UIButton *p4PlusFiveBtn;
+@property(retain) IBOutlet UIButton *p4MinusFiveBtn;
+@property(retain) IBOutlet UIButton *p4PlusOneBtn;
+@property(retain) IBOutlet UIButton *p4MinusOneBtn;
 
 @end
--- Lifecount/ViewController.m
+++ Lifecount/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#define buttonWidth 50
+#define buttonHeight 20
 
 @interface ViewController ()
 
@@ -27,6 +29,36 @@
     
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"background.jpg"]];
 
+    
+    
+    /*Botones Player 1*/
+    /*
+    UIButton *p1Plus5Button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    p1Plus5Button.frame = CGRectMake(160, 15, buttonWidth, buttonHeight);
+    [p1Plus5Button setTitle:@"+5" forState:UIControlStateNormal];
+    [p1Plus5Button addTarget:self action:@selector(plusFiveP1) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:p1Plus5Button];
+    
+    UIButton *p1Minus5Button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    p1Minus5Button.frame = CGRectMake(110, 15, buttonWidth, buttonHeight);
+    [p1Minus5Button setTitle:@"-5" forState:UIControlStateNormal];
+    [p1Minus5Button addTarget:self action:@selector(minusFiveP1) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:p1Minus5Button];
+    */
+    
+    /*
+     
+     UIImage *buttonImage = [UIImage imageNamed:@"ic_tab_codigoQR_unselected.png"];
+     UIImage *buttonImageSel = [UIImage imageNamed:@"ic_tab_codigoQR_selected.png"];
+     UIButton *aButton = [UIButton buttonWithType:UIButtonTypeCustom];
+     [aButton setImage:buttonImage forState:UIControlStateNormal];
+     [aButton setImage:buttonImageSel forState:UIControlEventTouchUpInside];
+     aButton.frame = CGRectMake(0.0, 0.0, buttonImage.size.width, buttonImage.size.height);
+     
+     // Set the Target and Action for aButton
+     [aButton addTarget:self action:@selector(readCodeButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+     
+     */
 	
     
 }
@@ -37,49 +69,60 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)plusOneP1:(id)sender
+- (IBAction)plusOneP1:(id)sender;
 {
     p1Lives += 1;
     p1LivesView.text = [NSString stringWithFormat:@"%d", p1Lives];
 }
 
--(IBAction)minusOneP1:(id)sender
+-(IBAction)minusOneP1:(id)sender;
 {
     p1Lives -= 1;
     p1LivesView.text = [NSString stringWithFormat:@"%d", p1Lives];
+    if(p1Lives < 1){
+        p1LivesView.enabled = false;
+        [_p1MinusFiveBtn setEnabled:false];
+        [_p1PlusFiveBtn setEnabled:false];
+        [_p1PlusOneBtn setEnabled:false];
+        [_p1MinusOneBtn setEnabled:false];
+        p1Lives = 0;
+        p1LivesView.text = [NSString stringWithFormat:@"%d", p1Lives];
+    }
+    else
+        p1LivesView.enabled = true;
 }
 
-- (IBAction)plusOneP2:(id)sender
+- (IBAction)plusOneP2:(id)sender;
 {
     p2Lives += 1;
     p2LivesView.text = [NSString stringWithFormat:@"%d", p2Lives];
 }
 
--(IBAction)minusOneP2:(id)sender
+-(IBAction)minusOneP2:(id)sender;
 {
     p2Lives -= 1;
     p2LivesView.text = [NSString stringWithFormat:@"%d", p2Lives];
 }
 
-- (IBAction)plusOneP3:(id)sender
+- (IBAction)plusOneP3:(id)sender;
 {
     p3Lives += 1;
     p3LivesView.text = [NSString stringWithFormat:@"%d", p3Lives];
 }
 
--(IBAction)minusOneP3:(id)sender
+-(IBAction)minusOneP3:(id)sender;
 {
     p3Lives -= 1;
     p3LivesView.text = [NSString stringWithFormat:@"%d", p3Lives];
 }
 
-- (IBAction)plusOneP4:(id)sender
+- (IBAction)plusOneP4:(id)sender;
 {
     p4Lives += 1;
     p4LivesView.text = [NSString stringWithFormat:@"%d", p4Lives];
 }
 
--(IBAction)minusOneP4:(id)sender
+-(IBAction)minusOneP4:(id)sender;
 {
     p4Lives -= 1;
     p4LivesView.text = [NSString stringWithFormat:@"%d", p4Lives];
@@ -87,55 +130,71 @@
 
 // Plus 5 methods
 
-- (IBAction)plusFiveP1:(id)sender
+- (IBAction)plusFiveP1:(id)sender;
 {
     p1Lives += 5;
     p1LivesView.text = [NSString stringWithFormat:@"%d", p1Lives];
+    if(p1Lives < 1){
+        p1LivesView.enabled = false;
+    }
+    else
+        p1LivesView.enabled = true;
 }
 
--(IBAction)minusFiveP1:(id)sender
+-(IBAction)minusFiveP1:(id)sender;
 {
     p1Lives -= 5;
     p1LivesView.text = [NSString stringWithFormat:@"%d", p1Lives];
+    if(p1Lives < 1){
+        p1LivesView.enabled = false;
+        [_p1MinusFiveBtn setEnabled:false];
+        [_p1PlusFiveBtn setEnabled:false];
+        [_p1PlusOneBtn setEnabled:false];
+        [_p1MinusOneBtn setEnabled:false];
+        p1Lives = 0;
+        p1LivesView.text = [NSString stringWithFormat:@"%d", p1Lives];
+    }
+    else
+        p1LivesView.enabled = true;
 }
 
-- (IBAction)plusFiveP2:(id)sender
+- (IBAction)plusFiveP2:(id)sender;
 {
     p2Lives += 5;
     p2LivesView.text = [NSString stringWithFormat:@"%d", p2Lives];
 }
 
--(IBAction)minusFiveP2:(id)sender
+-(IBAction)minusFiveP2:(id)sender;
 {
     p2Lives -= 5;
     p2LivesView.text = [NSString stringWithFormat:@"%d", p2Lives];
 }
 
-- (IBAction)plusFiveP3:(id)sender
+- (IBAction)plusFiveP3:(id)sender;
 {
     p3Lives += 5;
     p3LivesView.text = [NSString stringWithFormat:@"%d", p3Lives];
 }
 
--(IBAction)minusFiveP3:(id)sender
+-(IBAction)minusFiveP3:(id)sender;
 {
     p3Lives -= 5;
     p3LivesView.text = [NSString stringWithFormat:@"%d", p3Lives];
 }
 
-- (IBAction)plusFiveP4:(id)sender
+- (IBAction)plusFiveP4:(id)sender;
 {
     p4Lives += 5;
     p4LivesView.text = [NSString stringWithFormat:@"%d", p4Lives];
 }
 
--(IBAction)minusFiveP4:(id)sender
+-(IBAction)minusFiveP4:(id)sender;
 {
     p4Lives -= 5;
     p4LivesView.text = [NSString stringWithFormat:@"%d", p4Lives];
 }
 
--(IBAction)reset:(id)sender
+-(IBAction)reset
 {
     p1Lives = p2Lives = p3Lives = p4Lives = 20;
     p1LivesView.text = p2LivesView.text = p3LivesView.text = p4LivesView.text = [NSString stringWithFormat:@"%d", 20];
